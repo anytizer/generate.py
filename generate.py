27c14d713430de545443fe3d60152b548ffdfb23
Bimal Poudel <anytizer@users.noreply.github.com>
2020-11-29 12:47:40 -0700

    Update generate.py

--- generate.py
+++ generate.py
@@ -11,40 +11,80 @@
 # generate.bat
 #
 
-# Convert to test cases for these statements
+# Convert to test cases for these requirements
 cases = {
     "application": [
-        "id exist",
+        "id exists",
         "id does not exist",
-        "too old user cannot login"
+        "too old user cannot login",
+    ],
+    "password": [
+        "do not accept weak password()",
+        "change password(oldpassword, newpassword, confirmpassword)",
     ],
 }
+
+import re
+
 
 # Process
 # define classes
 # define class methods
+# handle input parameters
 # create unit tests
 
+class generator():
 
-def _testcase_body_template(case:str):
-    fname = case.lower().replace(" ", "_")
-    method = "something"
+    # do not accept => do_not_accept
+    def function_name(self, test_case: str):
+        function_name = test_case.lower().replace(" ", "_")
+        function_name = re.sub(r"\(.*?$", "", function_name, 0)
+        return function_name
+
+    # "", "get", "()", "get()", "get(a)", "get(a, b)"
+    # split parameters within ()
+    # insert "self" in the beginning
+    # get parameters
+    def variables(self, test_case: str):
+        parameters = ["self"]
+        parameter_string = ""
+
+        lpos = test_case.find("(")
+        rpos = test_case.find(")")
+
+        if lpos >= 0 and rpos >= 0:
+            parameter_string = case[lpos + 1:rpos]
+
+        parameters.extend(parameter_string.split(","))
+        variables = ", ".join([param for param in parameters if param])
+        return variables
+
+
+def _testcase_body_template(case: str):
+    function_name = generator().function_name(case)
+    variables = generator().variables(case)
+
+    default_method = "something"
     body = f"""
-    def test_{fname}(self):
+    def test_{function_name}({variables}):
         data = []
-        result = self.application.{method}(data)
-        self.assertTrue(result)
-"""
+        result = self.application.{default_method}(data)
+        self.assertTrue(result)"""
     return body
 
 
-for application, testcases in cases.items():
-    head = f"""#!/bin/python
+initials = f"""#!/bin/python
+# Warning: Auto generated code
+
 import unittest
-from application import application
+from .applications import application
+from .applications import password
+"""
 
-
-class TestGeneratedMethods(unittest.TestCase):
+print(initials)
+for application, testcases in cases.items():
+    head = f"""
+class TestGeneratedMethods{application.title()}(unittest.TestCase):
     application = None
 
     def setUp(self):
@@ -59,9 +99,8 @@ class TestGeneratedMethods(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        pass
-"""
+        pass"""
     print(head)
-    for tc in testcases:
-        body = _testcase_body_template(tc)
+    for case in testcases:
+        body = _testcase_body_template(case)
         print(body)
